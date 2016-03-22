beb0ca2d91dc2937838df0ef0cb8e59c370e6b86
Carl Peaslee <cpeaslee@gmail.com>
2016-03-22 11:33:46 -0500

    working

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -2,12 +2,12 @@ var myApp = angular.module("myApp", []);
 
 myApp.controller("SuperController", ["$scope", "$http", function($scope, $http){
 
-  $scope.studentArray = [];
+  $scope.studentArray;
 
   $scope.loadStudents = function () {
     $http.get("/students").then( function ( response ) {
-      $scope.studentArray.push(response.data);
-      console.log($scope.studentArray.students);
+      $scope.studentArray = response.data.students;
+      console.log($scope.studentArray);
     })
   };
 
